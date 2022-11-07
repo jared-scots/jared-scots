ad9ecdc753030e966ca8e1ec78399a86de67e846
jared-scots <117608133+jared-scots@users.noreply.github.com>
2022-11-07 13:14:44 +1300

    Add files via upload

---
+++ DellCommandUpdateSettings.ps1
@@ -0,0 +1,16 @@
+﻿$path = "C:\temp"
+If(!(test-path -PathType container $path))
+{
+      New-Item -ItemType Directory -Path $path
+}
+
+$url = "http://ibschoolsnz.org.nz/DellCommandUpdateSettings.xml"
+$outpath = "c:/temp/DellCommandUpdateSettings.xml"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+
+CD 'C:\Program Files\Dell\CommandUpdate'
+.\dcu-cli.exe /configure -importSettings=C:\temp\DellCommandUpdateSettings.xml 
+
